cec699d82c15faa7ac9481f5fe8e586a6acf87b4
Subhamoy Sarkar <subhamoysarkar526@gmail.com>
2024-08-21 20:12:48 +0530

    file arrangements and auth model

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express=require('express');
-const colors=require('colors');
-const dotenv=require('dotenv')
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js';
 
 //Configure env : 
 dotenv.config()
 // dotenv.config({{path:''}})  --> if dotevn file not in root directory
 
 
+// database config
+connectDB();
+
+
 //rest object
-const app=express()
+const app=express();
+
+
+//middlewares
+app.use(express.json());
+app.use(morgan(`dev`));
+
+//routes
+app.use('/api/v1/auth',authRoutes);
+
 
 //rest api
 app.get('/',(req,res)=>{
