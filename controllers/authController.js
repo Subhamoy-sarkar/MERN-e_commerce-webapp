7cafccdb9667c9ec326feda67b0ede779227557e
Subhamoy Sarkar <subhamoysarkar526@gmail.com>
2024-08-31 18:24:20 +0530

    Admin and User panel

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ import JWT from 'jsonwebtoken';
 import { comparePassword } from "../helpers/authHelper.js";
 export const registerController= async ( req , res)=>{
     try {
-        const {name,email,password , phone ,address}=req.body
+        const {name,email,password , phone ,address,answer}=req.body
         //validations
         if(!name){
             return res.send({message:"Name is required"});
@@ -21,6 +21,9 @@ export const registerController= async ( req , res)=>{
         if(!address){
             return res.send({message:"address is required"});
         }
+        if(!answer){
+            return res.send({message:"answer is required"});
+        }
 
         //Existing  user checking : 
         const existingUser=await userModel.findOne({email});
@@ -34,7 +37,7 @@ export const registerController= async ( req , res)=>{
         //register user:
         const hashedPassword=await hashPassword(password)
         //save
-        const user=await new userModel({name , email , phone , address , password:hashedPassword}).save();
+        const user=await new userModel({name , email , phone , address , password:hashedPassword ,answer}).save();
         res.status(201).send({
             success:true,
             message:"User register successfully",
@@ -87,7 +90,8 @@ export const loginController=async (req, res)=>{
                 name:user.name,
                 email:user.email,
                 phone:user.phone,
-                address:user.address
+                address:user.address,
+                role:user.role,
             },
             token,
         });
@@ -101,6 +105,47 @@ export const loginController=async (req, res)=>{
         }) 
     }
 };
+
+
+//forgotPasswordController
+export const forgotPasswordController=async(req,res)=>{
+    try{
+        const {email,answer,newpassword}=req.body;
+        if(!email){
+            res.status(400).send({message:"Email is required"});
+        }
+        if(!answer){
+            res.status(400).send({message:"answer is required"});
+        }
+        if(!newpassword){
+            res.status(400).send({message:"New password is required"});
+        }
+
+        //check
+        const user=await userModel.findOne({email,answer});
+        //validation
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:"wrong email or answer"
+            })
+        }
+        const hashed=await hashPassword(newpassword);
+        await userModel.findByIdAndUpdate(user._id,{password:hashed});
+        res.status(200).send({
+            success:true,
+            message:"password reset successfully",
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message:"Something is wrong",
+            error
+        })
+    }
+}
+
 
 //test controller
 export const testController=(req,res)=>{
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {registerController} from '../controllers/authController.js';
+import {forgotPasswordController, registerController} from '../controllers/authController.js';
 import { loginController } from '../controllers/authController.js';
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 import { testController } from '../controllers/authController.js';
@@ -15,12 +15,18 @@ router.post('/register', registerController);
 //LOGIN || POST
 router.post('/login',loginController);
 
+//Forgot password || POST
+router.post('/forgot-password',forgotPasswordController);
 
 //test routes
 router.get('/test',requireSignIn, isAdmin , testController);
 
-//protected route auth
+//protected route auth (user )
 router.get('/user-auth',requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true});
+})
+//protected route auth (admin)
+router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
     res.status(200).send({ok:true});
 })
 export default router
